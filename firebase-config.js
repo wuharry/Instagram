c62fcb27a3e716170a2a187f2412e7b850331c4c
wuharry <whw880218we@gmail.com>
2022-06-21 13:59:19 +0800

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvo14Ark9jWa7fVUnLGJZJXZThBfPrITI",
+    authDomain: "ig-test-9c7e7.firebaseapp.com",
+    projectId: "ig-test-9c7e7",
+    storageBucket: "ig-test-9c7e7.appspot.com",
+    messagingSenderId: "557728545517",
+    appId: "1:557728545517:web:c8f8ff0905c95c3cf82ae4",
+    measurementId: "G-N35NRMREGV"
+  };
+  
