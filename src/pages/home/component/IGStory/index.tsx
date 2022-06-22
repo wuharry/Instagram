8121d36fb228eba2388fd1cf527eee9814e243d7
wuharry <89292243+wuharry@users.noreply.github.com>
2022-06-22 08:28:24 +0800

    Update index.tsx

--- src/pages/home/component/IGStory/index.tsx
+++ src/pages/home/component/IGStory/index.tsx
@@ -3,7 +3,58 @@ import { useGetIGStoiesQuery } from "services/homeService"; //引入services 資
 import Item from "./item"; //引入ig動態球
 import Loading from "components/Loading";//引入Loading畫面
 
-
+const postdata3=[
+    {
+      "id": 1,
+      "name": "bruce_fe",
+      "avatar": "/images/avatars/a1.png"
+    },
+    {
+      "id": 2,
+      "name": "max",
+      "avatar": "/images/avatars/a2.png"
+    },
+    {
+      "id": 3,
+      "name": "fm",
+      "avatar": "/images/avatars/a3.png"
+    },
+    {
+      "id": 4,
+      "name": "joanne",
+      "avatar": "/images/avatars/a4.png"
+    },
+    {
+      "id": 5,
+      "name": "focus",
+      "avatar": "/images/avatars/a5.png"
+    },
+    {
+      "id": 6,
+      "name": "louis",
+      "avatar": "/images/avatars/a6.png"
+    },
+    {
+      "id": 7,
+      "name": "alvin",
+      "avatar": "/images/avatars/a7.png"
+    },
+    {
+      "id": 8,
+      "name": "grace",
+      "avatar": "/images/avatars/a8.png"
+    },
+    {
+      "id": 9,
+      "name": "rance",
+      "avatar": "/images/avatars/a9.png"
+    },
+    {
+      "id": 10,
+      "name": "bruce_fe",
+      "avatar": "/images/avatars/a10.png"
+    }
+  ]
 const IGStory: React.FC = () => {
   const { data, isLoading } = useGetIGStoiesQuery("all");
   // useGetIGStoiesQuery不能在"自訂義"Hooks以外的地方使用
@@ -18,7 +69,7 @@ const IGStory: React.FC = () => {
       )}
 
       {!isLoading &&
-        data?.map((item) => {
+        postdata3?.map((item) => {
           // 如果data(來自後端/目前不是)為非空值'?'，做map
           const { id, name, avatar } = item;
           return <Item key={id} name={name} avatar={avatar} />;
